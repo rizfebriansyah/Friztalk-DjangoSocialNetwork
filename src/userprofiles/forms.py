eb267666ce58c549866f807a527aed306797ff2a
rizfebriansyah <rizqi980202@gmail.com>
2022-10-18 00:36:16 +0800

    Revert "Friztalk"
    This reverts commit 102c4a2355a63393edcdf609783a84e00d30522e.

--- src/userprofiles/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Profile
-
-class ProfilePageModelForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('f_name', 'l_name', 'biography', 'profile_picture')
--- src/userprofiles/utils.py
+++
@@ -1,5 +0,0 @@
-import uuid
-
-def generate_random_character():
-    randomcode = str(uuid.uuid4())[:9].replace('-','').lower()
-    return randomcode 
